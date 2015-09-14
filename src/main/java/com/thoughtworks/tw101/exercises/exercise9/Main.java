0ae881a4dfa448f90643e7b4f668ec5c5a2531ac
Lucy Santano <lucysantano@sympatico.ca>
2015-09-13 21:49:01 -0400

    Completed exercise 9

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Main.java
@@ -18,6 +18,8 @@ public class Main {
         root.add("Sara");
         root.add("Casey");
 
+
+
         List<String> names = root.names();
 
         for (String name : names) {
--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,58 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node left;
+    private Node right;
+    List<String> names = new ArrayList<String>();
 
     public Node(String name) {
         this.name = name;
+        left = null;
+        right = null;
     }
 
     public void add(String nameOfNewNode) {
+        if(nameOfNewNode == name){
+            return;
+        }
+        else if(nameOfNewNode.compareTo(name)<0) {
+            if (left == null) {
+                left = new Node(nameOfNewNode);
+                return;
+            } else {
+                left.add(nameOfNewNode);
+            }
+        }
+        else if (nameOfNewNode.compareTo(name)>0){
+            if(right == null){
+                right = new Node(nameOfNewNode);
+                return;
+            }
+            else{
+                right.add(nameOfNewNode);
+            }
+
+        }
+
+
     }
 
     public List<String> names() {
-        return null;
+        inOrderTraversal(this);
+        return names;
+    }
+
+    private void inOrderTraversal(Node node) {
+        if(node.left != null){
+            inOrderTraversal(node.left);
+        }
+        names.add(node.name);
+        if(node.right != null){
+            inOrderTraversal(node.right);
+        }
     }
 }
